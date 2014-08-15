a1c32cf5a44ec861655c80cc1ed05012b580bb8d
Jared Grippe <jared@deadlyicon.com>
2014-08-15 10:39:20 -0700

    WIP

--- test/Array#clear.js
+++ test/Array#clear.js
@@ -3,7 +3,8 @@ require('../')('Array#clear')
 describe('Array#clear', function(){
   it('should empty the array', function(){
     var array = [1,2,3]
-    array.clear()
-    expect({ a: 'b' }).to.have.key('a');
+    expect( array.length  ).to.be(3)
+    expect( array.clear() ).to.be(array)
+    expect( array.length  ).to.be(0)
   });
 });
